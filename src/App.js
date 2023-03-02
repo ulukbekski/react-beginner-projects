03533c29779392aa20b1c0956aa32aaf299b268b
ulukbekski <88556249+ulukbekski@users.noreply.github.com>
2023-03-02 13:33:41 +0600

    fis

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import './index.scss';
-import 'aos/dist/aos.css';
 import React from 'react';
-import Aos from 'aos';
 
-function Details({title, children}) {
-  const [open, setOpen] = React.useState(false)
-  return(
-   <button onMouseOver={()=>setOpen(true)} onMouseOut={()=> setOpen(false)}>
-    {title}
-    {open&&<div className='children'>{children}</div>}
-   </button>
-  )
-}
-  function App() {
-    
-  return (
+
+function App() {
+    return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
         <h1>0</h1>
       </div>
-    
-      <Details className="button" title="Plus">
-          <h1 className='children'>1234</h1>
-        </Details>
-
-        <Details className="button" title="qwerty">
-          <h2 className='children'>qwerty</h2>
-        </Details>
+    <div className='flex'>
+      <button className="button">increment +</button>
+      <button className='button'>reset 0</button>
+      <button className="button">decrement -</button>
+    </div>
+      
     </div>
   );
 }
